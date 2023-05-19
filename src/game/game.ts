e49361ea6fd2dc882b9a76f786539a1cfffb3437
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-05-19 00:37:18 -0700

    chatgptville

---
+++ src/game/game.ts
@@ -0,0 +1,72 @@
+/**
+ *
+ */
+export type Game = {
+    lastUpdated: Date
+    npcs: Record<string, Npc>
+    worldState: WorldState
+}
+
+export type GameUpdate = {}
+export type TimeUpdate = {}
+export type WorldUpdate = {}
+export type NpcUpdate = {}
+
+export type GameEvent = {}
+
+type WorldState = {
+    preface: string
+    currentNpcChat: CurrentNpcChat
+}
+
+type Npc = {
+    name: string
+    bio: string
+    position: Position
+}
+
+type ChatMessage = {
+    message: string
+}
+
+type CurrentNpcChat = {
+    npcId: string
+    messageHistory: ChatMessage[]
+}
+
+type Position = {
+    x: number
+    y: number
+}
+
+export function newGame(preface: string): Game {
+    throw new Error("Not implemented") //todo
+}
+
+function createNpcs(): Npc[] {
+    return [newNpc(), newNpc()]
+}
+
+function newNpc(): Npc {
+    throw new Error("Not implemented") //todo
+}
+
+export function updateGame(game: Game, update: WorldUpdate): GameEvent[] {
+    game.lastUpdated = new Date()
+
+    throw new Error("Not implemented") //todo
+}
+
+export function startChat(game: Game, npcId: string) {
+    throw new Error("Not implemented") //todo
+}
+
+export function endChat(game: Game) {
+    throw new Error("Not implemented") //todo
+}
+
+export function continueChat(game: Game, playerMessage: String) {}
+
+export function endGame(game: Game) {
+    throw new Error("Not implemented") //todo
+}
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,64 @@
-import { hello } from "./hello.js"
+import { endGame, Game, GameUpdate, updateGame } from "./game/game.js"
+import { v4 as uuidv4 } from "uuid"
+import _fastify from "fastify"
 
-console.log(hello)
+const fastify = _fastify({ logger: true })
 
-throw new Error("are the source maps working?")
+/**
+ * sessionId : Game
+ */
+const games: Record<string, Game> = {}
+
+// Require the framework and instantiate it
+
+// Declare a route
+fastify.get("/", async (request, reply) => {
+    return { hello: "world" }
+})
+
+//starts a new game
+fastify.post("/start-game", async (request, reply) => {
+    let gameId = uuidv4()
+    reply.send({ gameId })
+})
+
+//ends a game if it exists
+fastify.post("/end-game", async (request, reply) => {
+    const { gameId } = request.body as any
+
+    const game = games[gameId]
+    if (game) {
+        endGame(game)
+    }
+
+    delete games[gameId]
+})
+
+//updates the world
+fastify.post("/update-game", async (request, reply) => {
+    const gameId = request.headers["game-id"] as string
+
+    const { update } = request.body as { update: GameUpdate }
+
+    const game = games[gameId]
+
+    if (!game) {
+        reply.status(404).send("Game not found")
+        return
+    }
+
+    const updateEvents = updateGame(game, update)
+
+    throw new Error("Not implemented") //todo return the response to update the world on the client
+})
+
+// Run the server!
+const start = async () => {
+    try {
+        await fastify.listen({ port: 3000 })
+    } catch (err) {
+        fastify.log.error(err)
+        process.exit(1)
+    }
+}
+start().then((r) => console.log("Server started!"))
