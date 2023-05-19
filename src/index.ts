572cac06e0d14bd6ee69569a271f742b8394efcd
dQw4w9WgXcQ <22358065+dQw4w9WgXcQ@users.noreply.github.com>
2023-05-18 21:18:38 -0700

    prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { hello } from "./hello.js";
+import { hello } from "./hello.js"
 
 console.log(hello)
 
-throw new Error("are the source maps working?");
+throw new Error("are the source maps working?")
